96341297b19ea6c5d98cc67fc7a12bf250558968
cybercorgiArbitrum <162130004+cybercorgiArbitrum@users.noreply.github.com>
2024-03-04 23:09:13 +0200

    Update CyberCorgi.sol

--- CyberCorgi.sol
+++ CyberCorgi.sol
@@ -2,36 +2,19 @@ pragma solidity >=0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+pragma solidity >=0.8.18;
+
 contract CyberCorgi is Ownable, ERC20 {
 
-    event Before(address _address, uint _balanceBeforeTransfer);
-
-    bool public _tradingStarted;
-    uint256 public maxHoldingAmount = 25_000_000_000 * 10 ** decimals(); //no more than 2.5% of total supply per address
     uint _totalSupply = 1_000_000_000_000 * 10 ** decimals();
 
-    constructor() ERC20("Cyber Corgi", "CYBRC") {
+    constructor() ERC20("Cyber Corgi", "CYBERCORGI") {
         _mint(msg.sender, _totalSupply);
-        _tradingStarted = true;
-    }
-
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) override internal virtual {
-      
-        
-        if (_tradingStarted) {
-            uint balanceOfReciever = balanceOf(to);
-            emit Before(to, balanceOfReciever);
-
-            require(balanceOfReciever + amount <= maxHoldingAmount , "Anti Whale Protection. Cannot have more than 1.8% ot total supply");
-        }
     }
 
     function burn(uint256 value) external {
         _burn(msg.sender, value);
     }
+
+    
 }
